181c95d2c561867cf5252af39dc1a266fcaff24c
Sergey Rodin <rodin.s@rambler.ru>
2020-05-12 07:12:49 +0300

    Add e2e-test addBag.

---
+++ app/src/androidTest/java/com/sergeyrodin/matchesboxes/MainActivityTest.kt
@@ -0,0 +1,56 @@
+package com.sergeyrodin.matchesboxes
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    private lateinit var componentsDataSource: ComponentsDataSource
+
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+
+    @Before
+    fun registerIdlingResource() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+    }
+
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+    }
+
+    @Before
+    fun init() {
+        componentsDataSource = ServiceLocator.provideComponentsDataSource(getApplicationContext())
+    }
+
+    @After
+    fun reset() {
+        ServiceLocator.resetDataSource()
+    }
+
+    @Test
+    fun addBag_BagNameDisplayed() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        onView(withId(R.id.add_bag_fab)).perform(click())
+        onView(withId(R.id.bag_name_edit)).perform(typeText("New bag"))
+        onView(withId(R.id.bag_save_fab)).perform(click())
+        onView(withText("New bag")).check(matches(isDisplayed()))
+    }
+
+}
