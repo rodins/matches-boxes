9fe0b7e8532213b80ddefaaec3b5d3a47a021ca7
Sergey Rodin <rodin.s@rambler.ru>
2020-05-24 16:43:33 +0300

    Back and Up bag navigation tests.

--- app/src/androidTest/java/com/sergeyrodin/matchesboxes/AppNavigationTest.kt
+++ app/src/androidTest/java/com/sergeyrodin/matchesboxes/AppNavigationTest.kt
@@ -1,14 +1,12 @@
 package com.sergeyrodin.matchesboxes
 
 import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
-import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.RootMatchers.withDecorView
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -17,7 +15,6 @@ import com.sergeyrodin.matchesboxes.util.DataBindingIdlingResource
 import com.sergeyrodin.matchesboxes.util.EspressoIdlingResource
 import com.sergeyrodin.matchesboxes.util.monitorActivity
 import kotlinx.coroutines.runBlocking
-import org.hamcrest.CoreMatchers.not
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -25,9 +22,8 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class MainActivityTest {
+class AppNavigationTest {
     private lateinit var dataSource: RadioComponentsDataSource
-
     private val dataBindingIdlingResource = DataBindingIdlingResource()
 
     @Before
@@ -44,7 +40,7 @@ class MainActivityTest {
 
     @Before
     fun init() {
-        dataSource = ServiceLocator.provideRadioComponentsDataSource(getApplicationContext())
+        dataSource = ServiceLocator.provideRadioComponentsDataSource(ApplicationProvider.getApplicationContext())
     }
 
     @After
@@ -53,59 +49,30 @@ class MainActivityTest {
     }
 
     @Test
-    fun addBag_bagNameDisplayed() = runBlocking{
+    fun addBag_navigateBack() = runBlocking{
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
         dataBindingIdlingResource.monitorActivity(activityScenario)
 
         onView(withId(R.id.add_bag_fab)).perform(click())
-        onView(withId(R.id.bag_edit)).perform(typeText("New bag"))
-        onView(withId(R.id.save_bag_fab)).perform(click())
-        onView(withText("New bag")).check(matches(isDisplayed()))
-        activityScenario.close()
-    }
-
-    @Test
-    fun addBag_deleteButtonIsNotDisplayed() = runBlocking{
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-        dataBindingIdlingResource.monitorActivity(activityScenario)
-
-        onView(withId(R.id.add_bag_fab)).perform(click())
-
-        onView(withId(R.id.action_delete)).check(doesNotExist())
-
-        activityScenario.close()
-    }
-
-    @Test
-    fun noBags_addBagClick_textEquals() = runBlocking{
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-        dataBindingIdlingResource.monitorActivity(activityScenario)
-
+        onView(withId(R.id.bag_edit)).check(matches(withHint(R.string.enter_bag_name)))
+        pressBack()
         onView(withId(R.id.no_bags_added_text)).check(matches(isDisplayed()))
+
+        activityScenario.close()
+    }
+
+    @Test
+    fun addBag_navigateUp() = runBlocking{
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
         onView(withId(R.id.add_bag_fab)).perform(click())
         onView(withId(R.id.bag_edit)).check(matches(withHint(R.string.enter_bag_name)))
 
-        activityScenario.close()
-    }
+        onView(withContentDescription(R.string.abc_action_bar_up_description)).perform(click())
 
-    @Test
-    fun addBag_showToast() = runBlocking{
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-        dataBindingIdlingResource.monitorActivity(activityScenario)
-        var activity: MainActivity? = null
-        activityScenario.onActivity{
-            activity = it
-        }
-
-        onView(withId(R.id.add_bag_fab)).perform(click())
-        onView(withId(R.id.bag_edit)).perform(typeText("New bag"))
-        onView(withId(R.id.save_bag_fab)).perform(click())
-
-        onView(withText(R.string.bag_added))
-            .inRoot(withDecorView(not(activity?.window?.decorView)))
-            .check(matches(isDisplayed()))
+        onView(withId(R.id.no_bags_added_text)).check(matches(isDisplayed()))
 
         activityScenario.close()
     }
-
 }
