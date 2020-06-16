4d795f425558d16c28d504d59c2dacdb785daeaf
Sergey Rodin <rodin.s@rambler.ru>
2020-06-16 19:22:54 +0300

    SearchBuyEditFragment quantity plus/minus buttons.

--- app/src/androidTest/java/com/sergeyrodin/matchesboxes/searchbuy/edit/SearchBuyEditFragmentTest.kt
+++ app/src/androidTest/java/com/sergeyrodin/matchesboxes/searchbuy/edit/SearchBuyEditFragmentTest.kt
@@ -4,6 +4,7 @@ import android.app.Service
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -69,5 +70,41 @@ class SearchBuyEditFragmentTest {
         launchFragmentInContainer<SearchBuyEditFragment>(bundle, R.style.AppTheme)
 
         onView(withText(component.quantity.toString())).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun quantityPlusClick_quantityEquals() {
+        val bag = Bag(1, "Bag")
+        val set = MatchesBoxSet(1, "Set", bag.id)
+        val box = MatchesBox(1, "Box", set.id)
+        val component = RadioComponent(1, "Component", 3,  box.id)
+        dataSource.addBags(bag)
+        dataSource.addMatchesBoxSets(set)
+        dataSource.addMatchesBoxes(box)
+        dataSource.addRadioComponents(component)
+        val bundle = SearchBuyEditFragmentArgs.Builder(component.id, "", true).build().toBundle()
+        launchFragmentInContainer<SearchBuyEditFragment>(bundle, R.style.AppTheme)
+
+        onView(withText(R.string.button_plus)).perform(click())
+
+        onView(withText((component.quantity + 1).toString())).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun quantityMinusClick_quantityEquals() {
+        val bag = Bag(1, "Bag")
+        val set = MatchesBoxSet(1, "Set", bag.id)
+        val box = MatchesBox(1, "Box", set.id)
+        val component = RadioComponent(1, "Component", 3,  box.id)
+        dataSource.addBags(bag)
+        dataSource.addMatchesBoxSets(set)
+        dataSource.addMatchesBoxes(box)
+        dataSource.addRadioComponents(component)
+        val bundle = SearchBuyEditFragmentArgs.Builder(component.id, "", true).build().toBundle()
+        launchFragmentInContainer<SearchBuyEditFragment>(bundle, R.style.AppTheme)
+
+        onView(withText(R.string.button_minus)).perform(click())
+
+        onView(withText((component.quantity - 1).toString())).check(matches(isDisplayed()))
     }
 }
