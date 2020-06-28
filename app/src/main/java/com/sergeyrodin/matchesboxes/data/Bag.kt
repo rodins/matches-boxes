cd004b564c56f11050ef89df5d0a91c14e955c5b
Sergey Rodin <rodin.s@rambler.ru>
2020-06-28 10:51:40 +0300

    Make bag parcelable.

--- app/src/main/java/com/sergeyrodin/matchesboxes/data/Bag.kt
+++ app/src/main/java/com/sergeyrodin/matchesboxes/data/Bag.kt
@@ -1,11 +1,14 @@
 package com.sergeyrodin.matchesboxes.data
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
 @Entity(tableName = "bags")
+@Parcelize
 data class Bag(
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0,
     var name: String
-)
+): Parcelable
