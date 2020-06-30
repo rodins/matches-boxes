bbdacb98c421719ceb260f996a7b8b6f755264ee
Sergey Rodin <rodin.s@rambler.ru>
2020-06-30 06:24:11 +0300

    Make MatchesBox.kt parcelable.

--- app/src/main/java/com/sergeyrodin/matchesboxes/data/MatchesBox.kt
+++ app/src/main/java/com/sergeyrodin/matchesboxes/data/MatchesBox.kt
@@ -1,25 +1,26 @@
 package com.sergeyrodin.matchesboxes.data
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.ForeignKey.CASCADE
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
 @Entity(tableName = "matches_boxes",
-    foreignKeys = arrayOf(
-        ForeignKey(
-            entity = MatchesBoxSet::class,
-            parentColumns = arrayOf("id"),
-            childColumns = arrayOf("matches_box_set_id"),
-            onDelete = CASCADE
-        )
-    )
+    foreignKeys = [ForeignKey(
+        entity = MatchesBoxSet::class,
+        parentColumns = ["id"],
+        childColumns = ["matches_box_set_id"],
+        onDelete = CASCADE
+    )]
 )
+@Parcelize
 data class MatchesBox(
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0,
     var name: String,
     @ColumnInfo(name = "matches_box_set_id", index = true)
     var matchesBoxSetId: Int
-)
+): Parcelable
