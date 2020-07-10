217acea91a7ff35a9fbe28c61a58ca0ed334d316
Sergey Rodin <rodin.s@rambler.ru>
2020-07-10 06:44:57 +0300

    Delete CommonViewModel.kt

--- app/src/main/java/com/sergeyrodin/matchesboxes/matchesbox/list/MatchesBoxListFragment.kt
+++ app/src/main/java/com/sergeyrodin/matchesboxes/matchesbox/list/MatchesBoxListFragment.kt
@@ -2,18 +2,13 @@ package com.sergeyrodin.matchesboxes.matchesbox.list
 
 import android.os.Bundle
 import android.view.*
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.sergeyrodin.matchesboxes.*
 import com.sergeyrodin.matchesboxes.bag.list.DisplayQuantityAdapter
 import com.sergeyrodin.matchesboxes.bag.list.DisplayQuantityListener
-import com.sergeyrodin.matchesboxes.common.list.CommonViewModel
-import com.sergeyrodin.matchesboxes.common.list.CommonViewModelFactory
 
 import com.sergeyrodin.matchesboxes.databinding.FragmentMatchesBoxListBinding
 
