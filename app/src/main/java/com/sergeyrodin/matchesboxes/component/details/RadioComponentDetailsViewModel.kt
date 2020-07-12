c4455388f7a916c13125039095f7257926a6b694
Sergey Rodin <rodin.s@rambler.ru>
2020-07-12 10:02:32 +0300

    Added RadioComponentDetailsViewModelFactory

--- app/src/main/java/com/sergeyrodin/matchesboxes/component/details/RadioComponentDetailsViewModel.kt
+++ app/src/main/java/com/sergeyrodin/matchesboxes/component/details/RadioComponentDetailsViewModel.kt
@@ -1,22 +1,31 @@
 package com.sergeyrodin.matchesboxes.component.details
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
-import androidx.lifecycle.switchMap
+import androidx.lifecycle.*
 import com.sergeyrodin.matchesboxes.data.RadioComponentDetails
 import com.sergeyrodin.matchesboxes.data.RadioComponentsDataSource
 
 class RadioComponentDetailsViewModel(dataSource: RadioComponentsDataSource): ViewModel() {
     private val componentId = MutableLiveData<Int>()
     val details = componentId.switchMap{ id ->
-        liveData<RadioComponentDetails>{
+        liveData{
             emit(dataSource.getRadioComponentDetailsById(id))
         }
     }
 
     fun start(id: Int) {
         componentId.value = id
+    }
+
+}
+
+class RadioComponentDetailsViewModelFactory(private val dataSource: RadioComponentsDataSource): ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(RadioComponentDetailsViewModel::class.java)) {
+            return RadioComponentDetailsViewModel(dataSource) as T
+        }else {
+            throw IllegalArgumentException("No ViewModel class found.")
+        }
     }
 
 }
