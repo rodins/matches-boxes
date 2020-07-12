9aebbec3842ab8b811c30ae65df0315e9e5e48f2
Sergey Rodin <rodin.s@rambler.ru>
2020-07-12 09:47:25 +0300

    Got details by component id in RadioComponentDetailsViewModel.kt

--- app/src/main/java/com/sergeyrodin/matchesboxes/component/details/RadioComponentDetailsViewModel.kt
+++ app/src/main/java/com/sergeyrodin/matchesboxes/component/details/RadioComponentDetailsViewModel.kt
@@ -1,8 +1,22 @@
 package com.sergeyrodin.matchesboxes.component.details
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.switchMap
+import com.sergeyrodin.matchesboxes.data.RadioComponentDetails
 import com.sergeyrodin.matchesboxes.data.RadioComponentsDataSource
 
 class RadioComponentDetailsViewModel(dataSource: RadioComponentsDataSource): ViewModel() {
+    private val componentId = MutableLiveData<Int>()
+    val details = componentId.switchMap{ id ->
+        liveData<RadioComponentDetails>{
+            emit(dataSource.getRadioComponentDetailsById(id))
+        }
+    }
+
+    fun start(id: Int) {
+        componentId.value = id
+    }
 
 }
--- app/src/test/java/com/sergeyrodin/matchesboxes/component/details/RadioComponentDetailsViewModelTest.kt
+++ app/src/test/java/com/sergeyrodin/matchesboxes/component/details/RadioComponentDetailsViewModelTest.kt
@@ -1,10 +1,13 @@
 package com.sergeyrodin.matchesboxes.component.details
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.sergeyrodin.matchesboxes.data.FakeDataSource
-import org.junit.Assert.*
+import com.sergeyrodin.matchesboxes.data.*
+import com.sergeyrodin.matchesboxes.getOrAwaitValue
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class RadioComponentDetailsViewModelTest {
     @get:Rule
@@ -19,6 +22,25 @@ class RadioComponentDetailsViewModelTest {
         subject = RadioComponentDetailsViewModel(dataSource)
     }
 
+    @Test
+    fun componentId_detailsEqual() {
+        val bag = Bag(1, "Bag")
+        val set = MatchesBoxSet(1, "Set", bag.id)
+        val box = MatchesBox(1, "Box", set.id)
+        val component = RadioComponent(1, "Component", 2, box.id)
+        dataSource.addBags(bag)
+        dataSource.addMatchesBoxSets(set)
+        dataSource.addMatchesBoxes(box)
+        dataSource.addRadioComponents(component)
 
+        subject.start(component.id)
+
+        val details = subject.details.getOrAwaitValue()
+        assertThat(details.bagName, `is`(bag.name))
+        assertThat(details.setName, `is`(set.name))
+        assertThat(details.boxName, `is`(box.name))
+        assertThat(details.componentName, `is`(component.name))
+        assertThat(details.componentQuantity, `is`(component.quantity.toString()))
+    }
 
 }
