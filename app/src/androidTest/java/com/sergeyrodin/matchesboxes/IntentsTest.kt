3a4a87d6e3ad55be1e59a199f01a5cd1bed9e4f3
Sergey Rodin <rodin.s@rambler.ru>
2020-07-18 08:19:00 +0300

    Intent test passed.

---
+++ app/src/androidTest/java/com/sergeyrodin/matchesboxes/IntentsTest.kt
@@ -0,0 +1,88 @@
+package com.sergeyrodin.matchesboxes
+
+import android.app.SearchManager
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.sergeyrodin.matchesboxes.data.*
+import com.sergeyrodin.matchesboxes.util.DataBindingIdlingResource
+import com.sergeyrodin.matchesboxes.util.EspressoIdlingResource
+import com.sergeyrodin.matchesboxes.util.monitorActivity
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class IntentsTest {
+    @get:Rule
+    val intentsTestRule = IntentsTestRule(MainActivity::class.java)
+    private lateinit var dataSource: RadioComponentsDataSource
+
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+
+    @Before
+    fun registerIdlingResource() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+    }
+
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+    }
+
+    @Before
+    fun init() {
+        dataSource = ServiceLocator.provideRadioComponentsDataSource(ApplicationProvider.getApplicationContext())
+    }
+
+    @After
+    fun reset() {
+        ServiceLocator.resetDataSource()
+    }
+
+    @Test
+    fun componentDetails_infoClick_intentCalled() = runBlocking {
+        val bag = Bag(1, "Bag")
+        val set = MatchesBoxSet(1, "Set", bag.id)
+        val box = MatchesBox(1, "Box", set.id)
+        val component = RadioComponent(1, "LA78041", 3, box.id)
+        dataSource.insertBag(bag)
+        dataSource.insertMatchesBoxSet(set)
+        dataSource.insertMatchesBox(box)
+        dataSource.insertRadioComponent(component)
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        onView(withText(bag.name)).perform(click())
+        onView(withText(set.name)).perform(click())
+        onView(withText(box.name)).perform(click())
+        onView(withText(component.name)).perform(click())
+        onView(withId(R.id.action_info)).perform(click())
+
+        Intents.intended(
+            CoreMatchers.allOf(
+                IntentMatchers.hasAction(CoreMatchers.equalTo(Intent.ACTION_WEB_SEARCH)),
+                IntentMatchers.hasExtra(SearchManager.QUERY, component.name)
+            )
+        )
+
+        activityScenario.close()
+    }
+}
