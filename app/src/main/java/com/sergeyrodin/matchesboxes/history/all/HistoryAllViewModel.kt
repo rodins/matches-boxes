05656e4faaf78f067fe2aaee4e5ddd04f1b9bb6d
Sergey Rodin <rodin.s@rambler.ru>
2020-07-28 06:51:10 +0300

    History view model factory.

--- app/src/main/java/com/sergeyrodin/matchesboxes/history/all/HistoryAllViewModel.kt
+++ app/src/main/java/com/sergeyrodin/matchesboxes/history/all/HistoryAllViewModel.kt
@@ -1,13 +1,26 @@
 package com.sergeyrodin.matchesboxes.history.all
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.map
 import com.sergeyrodin.matchesboxes.data.RadioComponentsDataSource
+import java.lang.IllegalArgumentException
 
 class HistoryAllViewModel(dataSource: RadioComponentsDataSource): ViewModel() {
     val historyList = dataSource.getHistoryList()
 
     val noHistoryTextVisible = historyList.map{
         it.isEmpty()
+    }
+}
+
+class HistoryAddViewModelFactory(private val dataSource: RadioComponentsDataSource): ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(HistoryAllViewModel::class.java)) {
+            return HistoryAllViewModel(dataSource) as T
+        }else {
+            throw IllegalArgumentException("No view mode class found.")
+        }
     }
 }
