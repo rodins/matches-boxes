dfbb02f8985b67a630152641ef46a8933a6d7ae2
Sergey Rodin <rodin.s@rambler.ru>
2020-07-29 08:16:18 +0300

    View model gives display history list.

--- app/src/main/java/com/sergeyrodin/matchesboxes/history/all/HistoryAllViewModel.kt
+++ app/src/main/java/com/sergeyrodin/matchesboxes/history/all/HistoryAllViewModel.kt
@@ -1,18 +1,39 @@
 package com.sergeyrodin.matchesboxes.history.all
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.map
+import androidx.lifecycle.*
 import com.sergeyrodin.matchesboxes.data.RadioComponentsDataSource
+import com.sergeyrodin.matchesboxes.util.convertLongToDateString
 import java.lang.IllegalArgumentException
 
 class HistoryAllViewModel(dataSource: RadioComponentsDataSource): ViewModel() {
-    val historyList = dataSource.getHistoryList()
+    private val historyList = dataSource.getHistoryList()
+
+    val displayHistoryList = historyList.switchMap{list ->
+        liveData{
+            val outputList = mutableListOf<DisplayHistory>()
+            list.forEach { history ->
+                val component = dataSource.getRadioComponentById(history.componentId)
+                val displayHistory = DisplayHistory(
+                    component?.name?:"",
+                    history.quantity.toString(),
+                    convertLongToDateString(history.date)
+                )
+                outputList.add(displayHistory)
+            }
+            emit(outputList)
+        }
+    }
 
     val noHistoryTextVisible = historyList.map{
         it.isEmpty()
     }
 }
+
+data class DisplayHistory(
+    var name: String,
+    var quantitiy: String,
+    var date: String
+)
 
 class HistoryAddViewModelFactory(private val dataSource: RadioComponentsDataSource): ViewModelProvider.Factory {
     @Suppress("unchecked_cast")
--- app/src/test/java/com/sergeyrodin/matchesboxes/history/all/HistoryAllViewModelTest.kt
+++ app/src/test/java/com/sergeyrodin/matchesboxes/history/all/HistoryAllViewModelTest.kt
@@ -38,7 +38,7 @@ class HistoryAllViewModelTest {
         dataSource.addRadioComponents(component)
         dataSource.addHistory(history1, history2, history3, history4)
 
-        val list = subject.historyList.getOrAwaitValue()
+        val list = subject.displayHistoryList.getOrAwaitValue()
         assertThat(list.size, `is`(4))
     }
 
@@ -50,7 +50,7 @@ class HistoryAllViewModelTest {
         dataSource.addRadioComponents()
         dataSource.addHistory()
 
-        val list = subject.historyList.getOrAwaitValue()
+        val list = subject.displayHistoryList.getOrAwaitValue()
         assertThat(list.size, `is`(0))
     }
 
@@ -84,5 +84,39 @@ class HistoryAllViewModelTest {
 
         val visible = subject.noHistoryTextVisible.getOrAwaitValue()
         assertThat(visible, `is`(false))
+    }
+
+    @Test
+    fun oneItem_componentNameEquals() {
+        val bag = Bag(1, "Bag")
+        val set = MatchesBoxSet(1, "Set", bag.id)
+        val box = MatchesBox(1, "Box", set.id)
+        val component = RadioComponent(1, "Component", 3, box.id)
+        val history = History(1, component.id, component.quantity)
+        dataSource.addBags(bag)
+        dataSource.addMatchesBoxSets(set)
+        dataSource.addMatchesBoxes(box)
+        dataSource.addRadioComponents(component)
+        dataSource.addHistory(history)
+
+        val list = subject.displayHistoryList.getOrAwaitValue()
+        assertThat(list[0].name, `is`(component.name))
+    }
+
+    @Test
+    fun oneItem_historyDateEquals() {
+        val bag = Bag(1, "Bag")
+        val set = MatchesBoxSet(1, "Set", bag.id)
+        val box = MatchesBox(1, "Box", set.id)
+        val component = RadioComponent(1, "Component", 3, box.id)
+        val history = History(1, component.id, component.quantity, 1595999582038L)
+        dataSource.addBags(bag)
+        dataSource.addMatchesBoxSets(set)
+        dataSource.addMatchesBoxes(box)
+        dataSource.addRadioComponents(component)
+        dataSource.addHistory(history)
+
+        val list = subject.displayHistoryList.getOrAwaitValue()
+        assertThat(list[0].date, `is`("середа лип.-29-2020 Time: 08:13"))
     }
 }
