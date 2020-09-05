9577391d0291d8e38f5840d56c0794d8700a58b1
Sergey Rodin <rodin.s@rambler.ru>
2020-09-05 10:21:03 +0300

    Added SearchViewModel.kt

---
+++ app/src/main/java/com/sergeyrodin/matchesboxes/search/SearchViewModel.kt
@@ -0,0 +1,34 @@
+package com.sergeyrodin.matchesboxes.search
+
+import androidx.lifecycle.*
+import com.sergeyrodin.matchesboxes.Event
+import com.sergeyrodin.matchesboxes.data.RadioComponentsDataSource
+
+class SearchViewModel(private val dataSource: RadioComponentsDataSource) : ViewModel() {
+    private val searchQuery = MutableLiveData<String>()
+    val items = searchQuery.switchMap{ query ->
+        liveData {
+            if (query.trim() != "") {
+                emit(dataSource.getRadioComponentsByQuery(query))
+            } else {
+                emit(listOf())
+            }
+        }
+    }
+
+    val noComponentsTextVisible = Transformations.map(items) {
+        it.isEmpty()
+    }
+
+    private val _addComponentEvent = MutableLiveData<Event<Unit>>()
+    val addComponentEvent: LiveData<Event<Unit>>
+        get() = _addComponentEvent
+
+    fun start(query: String) {
+        searchQuery.value = query
+    }
+
+    fun addComponent() {
+        _addComponentEvent.value = Event(Unit)
+    }
+}
---
+++ app/src/test/java/com/sergeyrodin/matchesboxes/search/SearchViewModelTest.kt
@@ -0,0 +1,81 @@
+package com.sergeyrodin.matchesboxes.search
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.sergeyrodin.matchesboxes.data.FakeDataSource
+import com.sergeyrodin.matchesboxes.data.RadioComponent
+import com.sergeyrodin.matchesboxes.getOrAwaitValue
+import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class SearchViewModelTest{
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var dataSource: FakeDataSource
+    private lateinit var subject: SearchViewModel
+
+    @Before
+    fun init() {
+        dataSource = FakeDataSource()
+        subject = SearchViewModel(dataSource)
+    }
+
+    @Test
+    fun queryInput_nameMatches() {
+        val boxId = 1
+        val component = RadioComponent(1, "Component", 3, boxId)
+        dataSource.addRadioComponents(component)
+        subject.start("compo")
+
+        val items = subject.items.getOrAwaitValue()
+        assertThat(items[0].name, `is`(component.name))
+    }
+
+    @Test
+    fun queryInput_nothingFound_sizeZero() {
+        val boxId = 1
+        val component = RadioComponent(1, "Component", 3, boxId)
+        dataSource.addRadioComponents(component)
+        subject.start("box")
+
+        val items = subject.items.getOrAwaitValue()
+        assertThat(items.size, `is`(0))
+    }
+
+    @Test
+    fun noComponentsTextVisible_nothingFound_equalsTrue() {
+        val boxId = 1
+        val component = RadioComponent(1, "Component", 3, boxId)
+        dataSource.addRadioComponents(component)
+        subject.start("box")
+
+        val isVisible = subject.noComponentsTextVisible.getOrAwaitValue()
+        assertThat(isVisible, `is`(true))
+    }
+
+    @Test
+    fun emptyQuery_nothingFound_equalsTrue() {
+        val boxId = 1
+        val component = RadioComponent(1, "Component", 3, boxId)
+        dataSource.addRadioComponents(component)
+        subject.start("")
+
+        val isVisible = subject.noComponentsTextVisible.getOrAwaitValue()
+        assertThat(isVisible, `is`(true))
+    }
+
+    @Test
+    fun noComponentsTextVisible_resultsFound_equalsFalse() {
+        val boxId = 1
+        val component = RadioComponent(1, "Component", 3, boxId)
+        dataSource.addRadioComponents(component)
+        subject.start("compo")
+
+        val isVisible = subject.noComponentsTextVisible.getOrAwaitValue()
+        assertThat(isVisible, `is`(false))
+    }
+}
