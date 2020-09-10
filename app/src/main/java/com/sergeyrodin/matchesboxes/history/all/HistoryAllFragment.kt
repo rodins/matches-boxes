4a4aac5a589c8b16767f9ff3bd2f0363c267567c
Sergey Rodin <rodin.s@rambler.ru>
2020-09-10 07:42:22 +0300

    HistoryAllFragment.kt refactoring.

--- app/src/main/java/com/sergeyrodin/matchesboxes/history/all/HistoryAllFragment.kt
+++ app/src/main/java/com/sergeyrodin/matchesboxes/history/all/HistoryAllFragment.kt
@@ -11,26 +11,53 @@ import com.sergeyrodin.matchesboxes.MatchesBoxesApplication
 import com.sergeyrodin.matchesboxes.databinding.FragmentHistoryAllBinding
 
 class HistoryAllFragment : Fragment() {
+    private lateinit var binding: FragmentHistoryAllBinding
+    private val viewModel by viewModels<HistoryAllViewModel> {
+        getViewModelFactory()
+    }
+
+    private fun getViewModelFactory(): HistoryAllViewModelFactory {
+        return HistoryAllViewModelFactory(
+            (requireContext().applicationContext as MatchesBoxesApplication).radioComponentsDataSource
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentHistoryAllBinding.inflate(inflater, container, false)
-        val viewModel by viewModels<HistoryAllViewModel> {
-            HistoryAllViewModelFactory(
-                (requireContext().applicationContext as MatchesBoxesApplication).radioComponentsDataSource
-            )
-        }
-        binding.lifecycleOwner = viewLifecycleOwner
-        binding.viewModel = viewModel
-        binding.displayHistoryList.adapter = DisplayHistoryAdapter(DisplayHistoryListener { id, name ->
-            findNavController().navigate(
-                HistoryAllFragmentDirections.actionHistoryAllFragmentToComponentHistoryFragment(id, name)
-            )
-        })
-
+        createBinding(inflater, container)
+        setupBinding()
         return binding.root
     }
+
+    private fun createBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ) {
+        binding = FragmentHistoryAllBinding.inflate(inflater, container, false)
+    }
+
+    private fun setupBinding() {
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+        binding.displayHistoryList.adapter = createDisplayHistoryAdapter()
+    }
+
+    private fun createDisplayHistoryAdapter(): DisplayHistoryAdapter {
+        return DisplayHistoryAdapter(DisplayHistoryListener { id, name ->
+            navigateToComponentHistoryFragment(id, name)
+        })
+    }
+
+    private fun navigateToComponentHistoryFragment(id: Int, name: String) {
+        findNavController().navigate(
+            HistoryAllFragmentDirections.actionHistoryAllFragmentToComponentHistoryFragment(
+                id,
+                name
+            )
+        )
+    }
+
 
 }
