74c13cb5d2919558641ad32beb53eca56295dac9
Sergey Rodin <rodin.s@rambler.ru>
2020-10-06 09:31:08 +0300

    ToastsTest.kt fixes.

--- app/src/androidTest/java/com/sergeyrodin/matchesboxes/ToastsTest.kt
+++ app/src/androidTest/java/com/sergeyrodin/matchesboxes/ToastsTest.kt
@@ -4,11 +4,16 @@ import android.view.View
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.RootMatchers
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.sergeyrodin.matchesboxes.data.*
@@ -17,6 +22,7 @@ import com.sergeyrodin.matchesboxes.util.EspressoIdlingResource
 import com.sergeyrodin.matchesboxes.util.monitorActivity
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.not
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -63,13 +69,13 @@ class ToastsTest {
             decorView = it.window.decorView
         }
 
-        Espresso.onView(ViewMatchers.withId(R.id.add_bag_fab)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.bag_edit)).perform(ViewActions.typeText("New bag"))
-        Espresso.onView(ViewMatchers.withId(R.id.save_bag_fab)).perform(ViewActions.click())
+        onView(withId(R.id.add_bag_fab)).perform(click())
+        onView(withId(R.id.bag_edit)).perform(ViewActions.typeText("New bag"))
+        onView(withId(R.id.save_bag_fab)).perform(click())
 
-        Espresso.onView(ViewMatchers.withText(R.string.bag_added))
+        onView(withText(R.string.bag_added))
             .inRoot(RootMatchers.withDecorView(CoreMatchers.not(decorView)))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .check(matches(isDisplayed()))
 
         activityScenario.close()
     }
@@ -84,15 +90,15 @@ class ToastsTest {
             decorView = it.window.decorView
         }
 
-        Espresso.onView(ViewMatchers.withText("Bag")).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.action_edit)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.bag_edit))
+        onView(withText("Bag")).perform(click())
+        onView(withId(R.id.action_edit)).perform(click())
+        onView(withId(R.id.bag_edit))
             .perform(ViewActions.replaceText("Bag updated"))
-        Espresso.onView(ViewMatchers.withId(R.id.save_bag_fab)).perform(ViewActions.click())
+        onView(withId(R.id.save_bag_fab)).perform(click())
 
-        Espresso.onView(ViewMatchers.withText(R.string.bag_updated))
-            .inRoot(RootMatchers.withDecorView(CoreMatchers.not(decorView)))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withText(R.string.bag_updated))
+            .inRoot(withDecorView(not(decorView)))
+            .check(matches(isDisplayed()))
 
         activityScenario.close()
     }
@@ -107,13 +113,13 @@ class ToastsTest {
             decorView = it.window.decorView
         }
 
-        Espresso.onView(ViewMatchers.withText("Bag")).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.action_edit)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.action_delete)).perform(ViewActions.click())
+        onView(withText("Bag")).perform(click())
+        onView(withId(R.id.action_edit)).perform(click())
+        onView(withId(R.id.action_delete)).perform(click())
 
-        Espresso.onView(ViewMatchers.withText(R.string.bag_deleted))
-            .inRoot(RootMatchers.withDecorView(CoreMatchers.not(decorView)))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withText(R.string.bag_deleted))
+            .inRoot(withDecorView(not(decorView)))
+            .check(matches(isDisplayed()))
 
         activityScenario.close()
     }
@@ -130,14 +136,14 @@ class ToastsTest {
             decorView = it.window.decorView
         }
 
-        Espresso.onView(ViewMatchers.withText("Bag")).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.add_set_fab)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.set_edit)).perform(ViewActions.typeText("MBS1"))
-        Espresso.onView(ViewMatchers.withId(R.id.save_set_fab)).perform(ViewActions.click())
+        onView(withText("Bag")).perform(click())
+        onView(withId(R.id.add_set_fab)).perform(click())
+        onView(withId(R.id.set_edit)).perform(ViewActions.typeText("MBS1"))
+        onView(withId(R.id.save_set_fab)).perform(click())
 
-        Espresso.onView(ViewMatchers.withText(R.string.matches_box_set_added))
-            .inRoot(RootMatchers.withDecorView(CoreMatchers.not(decorView)))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withText(R.string.matches_box_set_added))
+            .inRoot(withDecorView(not(decorView)))
+            .check(matches(isDisplayed()))
 
         activityScenario.close()
     }
@@ -155,16 +161,16 @@ class ToastsTest {
             decorView = it.window.decorView
         }
 
-        Espresso.onView(ViewMatchers.withText(bag.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withText(set.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.action_edit)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.set_edit))
+        onView(withText(bag.name)).perform(click())
+        onView(withText(set.name)).perform(click())
+        onView(withId(R.id.action_edit)).perform(click())
+        onView(withId(R.id.set_edit))
             .perform(ViewActions.replaceText("Set updated"))
-        Espresso.onView(ViewMatchers.withId(R.id.save_set_fab)).perform(ViewActions.click())
+        onView(withId(R.id.save_set_fab)).perform(click())
 
-        Espresso.onView(ViewMatchers.withText(R.string.matches_box_set_updated))
-            .inRoot(RootMatchers.withDecorView(CoreMatchers.not(decorView)))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withText(R.string.matches_box_set_updated))
+            .inRoot(withDecorView(not(decorView)))
+            .check(matches(isDisplayed()))
 
         activityScenario.close()
     }
@@ -182,14 +188,14 @@ class ToastsTest {
             decorView = it.window.decorView
         }
 
-        Espresso.onView(ViewMatchers.withText(bag.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withText(set.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.action_edit)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.action_delete)).perform(ViewActions.click())
+        onView(withText(bag.name)).perform(click())
+        onView(withText(set.name)).perform(click())
+        onView(withId(R.id.action_edit)).perform(click())
+        onView(withId(R.id.action_delete)).perform(click())
 
-        Espresso.onView(ViewMatchers.withText(R.string.matches_box_set_deleted))
-            .inRoot(RootMatchers.withDecorView(CoreMatchers.not(decorView)))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withText(R.string.matches_box_set_deleted))
+            .inRoot(withDecorView(not(decorView)))
+            .check(matches(isDisplayed()))
 
         activityScenario.close()
     }
@@ -209,16 +215,16 @@ class ToastsTest {
             decorView = it.window.decorView
         }
 
-        Espresso.onView(ViewMatchers.withText(bag.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withText(set.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.add_box_fab)).perform(ViewActions.click())
+        onView(withText(bag.name)).perform(click())
+        onView(withText(set.name)).perform(click())
+        onView(withId(R.id.add_box_fab)).perform(click())
 
-        Espresso.onView(ViewMatchers.withId(R.id.box_edit)).perform(ViewActions.typeText("New box"))
-        Espresso.onView(ViewMatchers.withId(R.id.save_box_fab)).perform(ViewActions.click())
+        onView(withId(R.id.box_edit)).perform(ViewActions.typeText("New box"))
+        onView(withId(R.id.save_box_fab)).perform(click())
 
-        Espresso.onView(ViewMatchers.withText(R.string.box_added))
-            .inRoot(RootMatchers.withDecorView(CoreMatchers.not(decorView)))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withText(R.string.box_added))
+            .inRoot(withDecorView(not(decorView)))
+            .check(matches(isDisplayed()))
 
         activityScenario.close()
     }
@@ -238,17 +244,17 @@ class ToastsTest {
             decorView = it.window.decorView
         }
 
-        Espresso.onView(ViewMatchers.withText(bag.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withText(set.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withText(box.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.action_edit)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.box_edit))
+        onView(withText(bag.name)).perform(click())
+        onView(withText(set.name)).perform(click())
+        onView(withText(box.name)).perform(click())
+        onView(withId(R.id.action_edit)).perform(click())
+        onView(withId(R.id.box_edit))
             .perform(ViewActions.replaceText("Updated box"))
-        Espresso.onView(ViewMatchers.withId(R.id.save_box_fab)).perform(ViewActions.click())
+        onView(withId(R.id.save_box_fab)).perform(click())
 
-        Espresso.onView(ViewMatchers.withText(R.string.box_updated))
-            .inRoot(RootMatchers.withDecorView(CoreMatchers.not(decorView)))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withText(R.string.box_updated))
+            .inRoot(withDecorView(not(decorView)))
+            .check(matches(isDisplayed()))
 
         activityScenario.close()
     }
@@ -268,15 +274,15 @@ class ToastsTest {
             decorView = it.window.decorView
         }
 
-        Espresso.onView(ViewMatchers.withText(bag.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withText(set.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withText(box.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.action_edit)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.action_delete)).perform(ViewActions.click())
+        onView(withText(bag.name)).perform(click())
+        onView(withText(set.name)).perform(click())
+        onView(withText(box.name)).perform(click())
+        onView(withId(R.id.action_edit)).perform(click())
+        onView(withId(R.id.action_delete)).perform(click())
 
-        Espresso.onView(ViewMatchers.withText(R.string.box_deleted))
-            .inRoot(RootMatchers.withDecorView(CoreMatchers.not(decorView)))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withText(R.string.box_deleted))
+            .inRoot(withDecorView(not(decorView)))
+            .check(matches(isDisplayed()))
 
         activityScenario.close()
     }
@@ -299,17 +305,17 @@ class ToastsTest {
             decorView = it.window.decorView
         }
 
-        Espresso.onView(ViewMatchers.withText(bag.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withText(set.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withText(box.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.add_component_fab)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.component_edit))
+        onView(withText(bag.name)).perform(click())
+        onView(withText(set.name)).perform(click())
+        onView(withText(box.name)).perform(click())
+        onView(withId(R.id.add_component_fab)).perform(click())
+        onView(withId(R.id.component_edit))
             .perform(ViewActions.typeText("Component"))
-        Espresso.onView(ViewMatchers.withId(R.id.save_component_fab)).perform(ViewActions.click())
+        onView(withId(R.id.save_component_fab)).perform(click())
 
-        Espresso.onView(ViewMatchers.withText(R.string.component_added))
-            .inRoot(RootMatchers.withDecorView(CoreMatchers.not(decorView)))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withText(R.string.component_added))
+            .inRoot(withDecorView(not(decorView)))
+            .check(matches(isDisplayed()))
 
         activityScenario.close()
     }
@@ -330,15 +336,15 @@ class ToastsTest {
             decorView = it.window.decorView
         }
 
-        Espresso.onView(ViewMatchers.withText(bag.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withText(set.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withText(box.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.add_component_fab)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.save_component_fab)).perform(ViewActions.click())
+        onView(withText(bag.name)).perform(click())
+        onView(withText(set.name)).perform(click())
+        onView(withText(box.name)).perform(click())
+        onView(withId(R.id.add_component_fab)).perform(click())
+        onView(withId(R.id.save_component_fab)).perform(click())
 
-        Espresso.onView(ViewMatchers.withText(R.string.save_component_error))
-            .inRoot(RootMatchers.withDecorView(CoreMatchers.not(decorView)))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withText(R.string.save_component_error))
+            .inRoot(withDecorView(not(decorView)))
+            .check(matches(isDisplayed()))
 
         activityScenario.close()
     }
@@ -361,18 +367,19 @@ class ToastsTest {
             decorView = it.window.decorView
         }
 
-        Espresso.onView(ViewMatchers.withText(bag.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withText(set.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withText(box.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withText(component.name)).perform(ViewActions.click())
+        onView(withText(bag.name)).perform(click())
+        onView(withText(set.name)).perform(click())
+        onView(withText(box.name)).perform(click())
+        onView(withText(component.name)).perform(click())
+        onView(withId(R.id.edit_component_fab)).perform(click())
 
-        Espresso.onView(ViewMatchers.withId(R.id.component_edit))
+        onView(withId(R.id.component_edit))
             .perform(ViewActions.typeText("Component updated"))
-        Espresso.onView(ViewMatchers.withId(R.id.save_component_fab)).perform(ViewActions.click())
+        onView(withId(R.id.save_component_fab)).perform(click())
 
-        Espresso.onView(ViewMatchers.withText(R.string.component_updated))
-            .inRoot(RootMatchers.withDecorView(CoreMatchers.not(decorView)))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withText(R.string.component_updated))
+            .inRoot(withDecorView(not(decorView)))
+            .check(matches(isDisplayed()))
 
         activityScenario.close()
     }
@@ -395,16 +402,17 @@ class ToastsTest {
             decorView = it.window.decorView
         }
 
-        Espresso.onView(ViewMatchers.withText(bag.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withText(set.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withText(box.name)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withText(component.name)).perform(ViewActions.click())
+        onView(withText(bag.name)).perform(click())
+        onView(withText(set.name)).perform(click())
+        onView(withText(box.name)).perform(click())
+        onView(withText(component.name)).perform(click())
+        onView(withId(R.id.edit_component_fab)).perform(click())
 
-        Espresso.onView(ViewMatchers.withId(R.id.action_delete)).perform(ViewActions.click())
+        onView(withId(R.id.action_delete)).perform(click())
 
-        Espresso.onView(ViewMatchers.withText(R.string.component_deleted))
-            .inRoot(RootMatchers.withDecorView(CoreMatchers.not(decorView)))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withText(R.string.component_deleted))
+            .inRoot(withDecorView(not(decorView)))
+            .check(matches(isDisplayed()))
 
         activityScenario.close()
     }
