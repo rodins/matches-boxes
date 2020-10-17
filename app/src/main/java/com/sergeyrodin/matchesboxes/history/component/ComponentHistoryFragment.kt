abf43011cfb73a894237a9751862f662a64c714f
Sergey Rodin <rodin.s@rambler.ru>
2020-10-17 07:42:03 +0300

    Extract ComponentHistoryActionModeController.kt from ComponentHistoryFragment.kt

--- app/src/main/java/com/sergeyrodin/matchesboxes/history/component/ComponentHistoryFragment.kt
+++ app/src/main/java/com/sergeyrodin/matchesboxes/history/component/ComponentHistoryFragment.kt
@@ -13,41 +13,7 @@ import com.sergeyrodin.matchesboxes.databinding.FragmentComponentHistoryBinding
 import com.sergeyrodin.matchesboxes.history.all.HistoryPresentationClickListener
 
 class ComponentHistoryFragment : Fragment() {
-    private var actionMode: ActionMode? = null
-    private val actionModeCallback = createActionModeCallback()
-
-    private fun createActionModeCallback(): ActionMode.Callback {
-        return object : ActionMode.Callback {
-            override fun onCreateActionMode(mode: ActionMode, menu: Menu): Boolean {
-                inflateMenu(mode, menu)
-                mode.title = getString(R.string.delete)
-                return true
-            }
-
-            override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
-                return false
-            }
-
-            override fun onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean {
-                if (item.itemId == R.id.action_delete) {
-                    viewModel.deleteHighlightedPresentation()
-                    return true
-                }
-                return false
-            }
-
-            override fun onDestroyActionMode(mode: ActionMode?) {
-                viewModel.actionModeClosed()
-                actionMode = null
-            }
-        }
-    }
-
-    private fun inflateMenu(mode: ActionMode, menu: Menu) {
-        val inflater: MenuInflater = mode.menuInflater
-        inflater.inflate(R.menu.delete_menu, menu)
-    }
-
+    private lateinit var actionModeController: ComponentHistoryActionModeController
     private lateinit var binding: FragmentComponentHistoryBinding
     private val args by navArgs<ComponentHistoryFragmentArgs>()
     private val viewModel by viewModels<ComponentHistoryViewModel> {
@@ -65,6 +31,7 @@ class ComponentHistoryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         createBinding(inflater)
+        createActionModeController()
         startViewModel()
         createAdapter()
         setupBinding()
@@ -75,6 +42,10 @@ class ComponentHistoryFragment : Fragment() {
 
     private fun createBinding(inflater: LayoutInflater) {
         binding = FragmentComponentHistoryBinding.inflate(inflater)
+    }
+
+    private fun createActionModeController() {
+        actionModeController = ComponentHistoryActionModeController(requireActivity(), viewModel)
     }
 
     private fun startViewModel() {
@@ -114,20 +85,10 @@ class ComponentHistoryFragment : Fragment() {
     private fun observeActionModeEvent() {
         viewModel.actionModeEvent.observe(viewLifecycleOwner, Observer { activateActionMode ->
             if (activateActionMode) {
-                startActionMode()
+                actionModeController.startActionMode()
             } else {
-                finishActionMode()
+                actionModeController.finishActionMode()
             }
         })
-    }
-
-    private fun startActionMode() {
-        if (actionMode == null) {
-            actionMode = activity?.startActionMode(actionModeCallback)
-        }
-    }
-
-    private fun finishActionMode() {
-        actionMode?.finish()
     }
 }
