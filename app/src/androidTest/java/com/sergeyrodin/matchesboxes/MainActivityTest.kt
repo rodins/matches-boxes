ca9564bc2edee92be7a9247328642bbba0a13d56
Sergey Rodin <rodin.s@rambler.ru>
2020-10-20 08:39:47 +0300

    Added bagsListFragment to bottom_nav_menu.xml

--- app/src/androidTest/java/com/sergeyrodin/matchesboxes/MainActivityTest.kt
+++ app/src/androidTest/java/com/sergeyrodin/matchesboxes/MainActivityTest.kt
@@ -2367,7 +2367,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun bottonNavigationDisplayed() {
+    fun bottomNavigationDisplayed() {
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
         dataBindingIdlingResource.monitorActivity(activityScenario)
 
@@ -2375,4 +2375,40 @@ class MainActivityTest {
 
         activityScenario.close()
     }
+
+    @Test
+    fun bottomNavigationBagsListFragmentDisplayed() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        onView(withId(R.id.bagsListFragment)).check(matches(isDisplayed()))
+
+        activityScenario.close()
+    }
+
+    @Test
+    fun bagsListFragmentClick_bagNameDisplayed() = runBlocking{
+        val bag = Bag(1, "Bag")
+        val set = MatchesBoxSet(1, "Set", bag.id)
+        val box = MatchesBox(1, "Box", set.id)
+        val component = RadioComponent(1, "LA78041", 1, box.id)
+        val history = History(1, component.id, component.quantity)
+        dataSource.insertBag(bag)
+        dataSource.insertMatchesBoxSet(set)
+        dataSource.insertMatchesBox(box)
+        dataSource.insertRadioComponent(component)
+        dataSource.insertHistory(history)
+
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        onView(withId(R.id.popularComponentsFragment)).perform(click())
+        onView(withId(R.id.bagsListFragment)).perform(click())
+
+        onView(withText(bag.name)).check(matches(isDisplayed()))
+
+        activityScenario.close()
+    }
+
+
 }
