322e92fa0ee84d79973595437282e451dc0fdcec
Sergey Rodin <rodin.s@rambler.ru>
2020-10-25 12:55:26 +0200

    NeededComponentsFragment.kt refactoring.

--- app/src/main/java/com/sergeyrodin/matchesboxes/needed/NeededComponentsFragment.kt
+++ app/src/main/java/com/sergeyrodin/matchesboxes/needed/NeededComponentsFragment.kt
@@ -18,43 +18,70 @@ import com.sergeyrodin.matchesboxes.component.list.RadioComponentListener
 import com.sergeyrodin.matchesboxes.databinding.FragmentNeededComponentsBinding
 
 class NeededComponentsFragment : Fragment() {
+    private val viewModel by viewModels<NeededComponentsViewModel> {
+        createViewModelFactory()
+    }
+
+    private fun createViewModelFactory(): NeededComponentsViewModelFactory {
+        return NeededComponentsViewModelFactory(
+            (requireContext().applicationContext as MatchesBoxesApplication).radioComponentsDataSource
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentNeededComponentsBinding.inflate(inflater, container, false)
-        val viewModel by viewModels<NeededComponentsViewModel> {
-            NeededComponentsViewModelFactory(
-                (requireContext().applicationContext as MatchesBoxesApplication).radioComponentsDataSource
-            )
-        }
+        val binding = createBinding(inflater, container)
+        setupBinding(binding)
+        observeAddComponentEvent()
+        return binding.root
+    }
 
+    private fun createBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ) = FragmentNeededComponentsBinding.inflate(inflater, container, false)
+
+    private fun setupBinding(binding: FragmentNeededComponentsBinding) {
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = viewModel
-        binding.items.adapter = RadioComponentAdapter(RadioComponentListener { componentId ->
-            findNavController().navigate(
-                NeededComponentsFragmentDirections
-                    .actionNeededComponentsFragmentToRadioComponentDetailsFragment(
-                        componentId,
-                        "",
-                        RadioComponentManipulatorReturns.TO_NEEDED_LIST)
-            )
-        })
+        binding.items.adapter = createAdapter()
+    }
 
-        viewModel.addComponentEvent.observe(viewLifecycleOwner, EventObserver{
-            findNavController().navigate(
-                NeededComponentsFragmentDirections
-                    .actionNeededComponentsFragmentToAddEditDeleteRadioComponentFragment(
-                    ADD_NEW_ITEM_ID,
-                        NO_ID_SET,
-                        getString(R.string.add_component),
-                        "",
-                        RadioComponentManipulatorReturns.TO_NEEDED_LIST
+    private fun createAdapter(): RadioComponentAdapter {
+        return RadioComponentAdapter(RadioComponentListener { componentId ->
+            navigateToRadioComponentDetailsFragment(componentId)
+        })
+    }
+
+    private fun navigateToRadioComponentDetailsFragment(componentId: Int) {
+        findNavController().navigate(
+            NeededComponentsFragmentDirections
+                .actionNeededComponentsFragmentToRadioComponentDetailsFragment(
+                    componentId,
+                    "",
+                    RadioComponentManipulatorReturns.TO_NEEDED_LIST
                 )
-            )
-        })
+        )
+    }
 
-        return binding.root
+    private fun observeAddComponentEvent() {
+        viewModel.addComponentEvent.observe(viewLifecycleOwner, EventObserver {
+            navigateToAddComponentFragment()
+        })
+    }
+
+    private fun navigateToAddComponentFragment() {
+        findNavController().navigate(
+            NeededComponentsFragmentDirections
+                .actionNeededComponentsFragmentToAddEditDeleteRadioComponentFragment(
+                    ADD_NEW_ITEM_ID,
+                    NO_ID_SET,
+                    getString(R.string.add_component),
+                    "",
+                    RadioComponentManipulatorReturns.TO_NEEDED_LIST
+                )
+        )
     }
 }
