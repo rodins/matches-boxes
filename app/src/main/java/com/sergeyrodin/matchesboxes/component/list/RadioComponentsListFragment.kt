395807e22f7bfa54c1b3dafef76fbc7668d07554
Sergey Rodin <rodin.s@rambler.ru>
2020-10-25 13:07:05 +0200

    RadioComponentsListFragment.kt refactoring.

--- app/src/main/java/com/sergeyrodin/matchesboxes/component/list/RadioComponentsListFragment.kt
+++ app/src/main/java/com/sergeyrodin/matchesboxes/component/list/RadioComponentsListFragment.kt
@@ -12,49 +12,76 @@ import com.sergeyrodin.matchesboxes.databinding.FragmentRadioComponentsListBindi
 
 class RadioComponentsListFragment : Fragment() {
     private val args by navArgs<RadioComponentsListFragmentArgs>()
+    private val viewModel by viewModels<RadioComponentsListViewModel>{
+        createViewModelFactory()
+    }
+
+    private fun createViewModelFactory(): RadioComponentsListViewModelFactory {
+        return RadioComponentsListViewModelFactory(
+            (requireContext().applicationContext as MatchesBoxesApplication).radioComponentsDataSource
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentRadioComponentsListBinding.inflate(inflater)
-        val viewModel by viewModels<RadioComponentsListViewModel>{
-            RadioComponentsListViewModelFactory(
-                (requireContext().applicationContext as MatchesBoxesApplication).radioComponentsDataSource
-            )
-        }
-
-        viewModel.startComponent(args.boxId)
-
-        binding.lifecycleOwner = viewLifecycleOwner
-        binding.viewModel = viewModel
-        binding.items.adapter = RadioComponentAdapter(RadioComponentListener { id ->
-            findNavController().navigate(
-                RadioComponentsListFragmentDirections
-                    .actionRadioComponentsListFragmentToRadioComponentDetailsFragment(
-                        id,
-                        "",
-                        RadioComponentManipulatorReturns.TO_COMPONENTS_LIST
-                    )
-            )
-        })
-
-        viewModel.addComponentEvent.observe(viewLifecycleOwner, EventObserver{
-            findNavController().navigate(
-                RadioComponentsListFragmentDirections
-                    .actionRadioComponentsListFragmentToAddEditDeleteRadioComponentFragment(
-                        ADD_NEW_ITEM_ID,
-                        args.boxId,
-                        getString(R.string.add_component),
-                        "",
-                        RadioComponentManipulatorReturns.TO_COMPONENTS_LIST
-                    )
-            )
-        })
+        val binding = createBinding(inflater)
+        startViewModel()
+        setupBinding(binding)
+        observeAddComponentEvent()
 
         setHasOptionsMenu(true)
-
         return binding.root
+    }
+
+    private fun createBinding(inflater: LayoutInflater) =
+        FragmentRadioComponentsListBinding.inflate(inflater)
+
+    private fun startViewModel() {
+        viewModel.startComponent(args.boxId)
+    }
+
+    private fun setupBinding(binding: FragmentRadioComponentsListBinding) {
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+        binding.items.adapter = createAdapter()
+    }
+
+    private fun createAdapter(): RadioComponentAdapter {
+        return RadioComponentAdapter(RadioComponentListener { id ->
+            navigateToDetailsFragment(id)
+        })
+    }
+
+    private fun navigateToDetailsFragment(id: Int) {
+        findNavController().navigate(
+            RadioComponentsListFragmentDirections
+                .actionRadioComponentsListFragmentToRadioComponentDetailsFragment(
+                    id,
+                    "",
+                    RadioComponentManipulatorReturns.TO_COMPONENTS_LIST
+                )
+        )
+    }
+
+    private fun observeAddComponentEvent() {
+        viewModel.addComponentEvent.observe(viewLifecycleOwner, EventObserver {
+            navigateToAddFragment()
+        })
+    }
+
+    private fun navigateToAddFragment() {
+        findNavController().navigate(
+            RadioComponentsListFragmentDirections
+                .actionRadioComponentsListFragmentToAddEditDeleteRadioComponentFragment(
+                    ADD_NEW_ITEM_ID,
+                    args.boxId,
+                    getString(R.string.add_component),
+                    "",
+                    RadioComponentManipulatorReturns.TO_COMPONENTS_LIST
+                )
+        )
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -64,13 +91,18 @@ class RadioComponentsListFragment : Fragment() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(item.itemId == R.id.action_edit) {
-            findNavController().navigate(
-                RadioComponentsListFragmentDirections
-                    .actionRadioComponentsListFragmentToAddEditDeleteMatchesBoxFragment(
-                        DO_NOT_NEED_THIS_VARIABLE, args.boxId, getString(R.string.update_box))
-            )
+            navigateToEditMatchesBoxFragment()
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun navigateToEditMatchesBoxFragment() {
+        findNavController().navigate(
+            RadioComponentsListFragmentDirections
+                .actionRadioComponentsListFragmentToAddEditDeleteMatchesBoxFragment(
+                    DO_NOT_NEED_THIS_VARIABLE, args.boxId, getString(R.string.update_box)
+                )
+        )
     }
 }
