b7879fe8a5de73bd0a2f4a62cc21848bcdce4af6
Sergey Rodin <rodin.s@rambler.ru>
2020-11-02 06:32:29 +0200

    Use FragmentContainerView in MainActivity.kt

--- app/src/main/java/com/sergeyrodin/matchesboxes/MainActivity.kt
+++ app/src/main/java/com/sergeyrodin/matchesboxes/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.sergeyrodin.matchesboxes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.findNavController
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
 
 const val ADD_NEW_ITEM_ID = -1
 const val DO_NOT_NEED_THIS_VARIABLE = -1
@@ -16,14 +15,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navController = findNavController(R.id.my_nav_host_fragment)
+        val navController = getNavController()
         NavigationUI.setupActionBarWithNavController(this, navController)
         findViewById<BottomNavigationView>(R.id.bottom_nav)
             .setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.my_nav_host_fragment)
+        val navController = getNavController()
         return navController.navigateUp()
+    }
+
+    private fun getNavController(): NavController {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment
+        return navHostFragment.navController
     }
 }
